3edf867d477d1c1dde34790df766de8cd7ac3e79
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-06-22 18:26:24 +0200

    Preparo métodos para insertar recetas y para actualizarlas

--- server/src/controllers/recetas.js
+++ server/src/controllers/recetas.js
@@ -1,14 +1,30 @@
 class RecetasController {
+    // Muestra todas las recetas
     recetas (request, response, next) {
         return response
             .status(200)
             .json({key: 'value'})
     }
 
+    // Muestra la receta recibida en el parámetro id
     receta (request, response, next) {
         return response
             .status(200)
             .json({id: request.params.id})
+    }
+
+    // Añade una nueva receta mediante petición POST
+    create (request, response, next) {
+        return response
+            .status(201)
+            .json({key: 'value POST'})
+    }
+
+    // Actualiza una receta ya existente mediante petición PUT
+    update (request, response, next) {
+        return response
+            .status(200)
+            .json({key: `value PUT con parámetro: ${request.params.id}`})
     }
 }
 
--- server/src/routes/recetas.js
+++ server/src/routes/recetas.js
@@ -9,9 +9,11 @@ const router = express.Router()
 // Ruta para acceder a la raíz partiendo de "/recetas".
 router.route('/')
     .get(RecetaController.recetas)
+    .post(RecetaController.create)
 
 // Ruta para acceder a un elemento concreto por su id.
 router.route('/:id')
     .get(RecetaController.receta)
+    .put(RecetaController.update)
 
 export default router
